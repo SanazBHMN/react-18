1f256b068277fce074a0968ae3b12cbf7db51daf
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-16 11:46:33 +0330

    DOM event handler

--- src/Counter.js
+++ src/Counter.js
@@ -3,13 +3,27 @@ import { useState } from "react";
 const Counter = (props) => {
   const [counter, setCounter] = useState(props.startAt);
 
-  setTimeout(() => setCounter(counter + props.countBy), 1000);
+  // setTimeout(() => setCounter(counter + props.countBy), 1000);
+
+  const countUp = () => {
+    setCounter(counter + props.countBy);
+  };
+
+  const countDown = () => {
+    setCounter(counter - props.countBy);
+  };
 
   return (
     <>
       <p>Start At: {props.startAt}</p>
       <p>Count By: {props.countBy}</p>
       <h4>{counter}</h4>
+      <button className="btn btn-success" onClick={countUp}>
+        Count Up
+      </button>
+      <button className="btn btn-danger" onClick={countDown}>
+        Count Down
+      </button>
     </>
   );
 };
