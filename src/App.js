bf088374e7077bfab214dac4d90d2f803fab3906
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-22 14:56:08 +0330

    Styling react with tailwindcss classes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const showEmployees = true;
 
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <>
           <input
@@ -16,9 +16,22 @@ const App = () => {
               setRole(e.target.value);
             }}
           />
-          <Employee name="Caleb" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Caleb"
+              role="Intern"
+              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
+            />
+            <Employee
+              name="Abby"
+              role={role}
+              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
+            />
+            <Employee
+              name="John"
+              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
+            />
+          </div>
         </>
       ) : (
         <p>You cannot see the employees</p>
