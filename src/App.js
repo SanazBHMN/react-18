d21717c06ae28fd2683af486f7afc4b7d0879fc8
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-22 15:26:13 +0330

    Map through state array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from "react";
 import Employee from "./components/Employee";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Devrel Engineer",
+      image:
+        "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d",
+    },
+    {
+      name: "Sal",
+      role: "Manager",
+      image:
+        "https://img.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg?w=996&t=st=1666438847~exp=1666439447~hmac=c70e677548dfe086856c8fcc15c78eeb9f665d301521aa113933a23e5bb1aaae",
+    },
+    {
+      name: "John",
+      role: "Director of Eng.",
+      image:
+        "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?w=996&t=st=1666439221~exp=1666439821~hmac=02d48ed9e55b2dd25a7a8200b784b89f2ba3fdd37a434bd2dcf0685688e83495",
+    },
+    {
+      name: "Melanie",
+      role: "Software Engineer",
+      image:
+        "https://img.freepik.com/free-photo/studio-portrait-successful-young-businesswoman_1262-5844.jpg?w=996&t=st=1666438947~exp=1666439547~hmac=ed5fa50b0b365c98b87ddb2d91b3519707729a33b3efcebcb165ce68a76f8b03",
+    },
+    {
+      name: "Corey",
+      role: "The devops guy",
+      image:
+        "https://img.freepik.com/free-photo/portrait-handsome-bearded-european-man-with-grey-hair-beard-smiles-pleasantly-looks-directly-front-being-good-mood-has-lucky-day-wears-spectacles-sweater-isolated-blue-wall_273609-44285.jpg?w=996&t=st=1666438973~exp=1666439573~hmac=f1ccb531b6250eea6b83f8505ec79a9bd925fef05338f72a432cb84f3b14a595",
+    },
+    {
+      name: "Jake",
+      role: "Senior Intern",
+      image:
+        "https://img.freepik.com/free-photo/relaxed-friendly-good-looking-european-guy-with-bristle-smiling-joyfully-with-white-healthy-teeth-holding-hands-pockets-being-happy-satisfied-posing-cheerfully-white-wall_176420-35904.jpg?w=996&t=st=1666439262~exp=1666439862~hmac=4bc485c278d6447c9278f2d5d7086ce02e7e472ec0d266c92f602526bcdd54f6",
+    },
+  ]);
   const showEmployees = true;
 
   return (
@@ -17,20 +56,14 @@ const App = () => {
             }}
           />
           <div className="flex flex-wrap justify-center">
-            <Employee
-              name="Caleb"
-              role="Intern"
-              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
-            />
-            <Employee
-              name="Abby"
-              role={role}
-              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
-            />
-            <Employee
-              name="John"
-              image="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=996&t=st=1666437529~exp=1666438129~hmac=53e75d2573ad3e0a5512f5c8e4f49d9d44e9fca64c5fbfbbdedf3ec822fa9d1d"
-            />
+            {employees.map((employee) => (
+              <Employee
+                key={uuidv4()}
+                name={employee.name}
+                role={employee.role}
+                image={employee.image}
+              />
+            ))}
           </div>
         </>
       ) : (
