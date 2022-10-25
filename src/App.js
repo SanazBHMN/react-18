9bbaf9fa4804474c7ce90e0f06a53c491ac94375
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-25 15:36:06 +0330

    Create a navbar with tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Employee from "./components/Employee";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
+import Header from "./components/Header";
 
 const App = () => {
   const [role, setRole] = useState("dev");
@@ -74,17 +75,11 @@ const App = () => {
   }
 
   return (
-    <div className="App">
+    <div className="App bg-gray-300 min-h-screen">
+      <Header />
       {showEmployees ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              // console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          />
-          <div className="flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center my-2">
             {employees.map((employee) => {
               const editEmployee = (
                 <EditEmployee
