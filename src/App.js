1998b63ccf504c72ba1e6934b1eb0db800f44e4a
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-26 14:10:55 +0330

    Routing with react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Employees from "./pages/Employees";
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/employees" element={<Employees />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 };
 
