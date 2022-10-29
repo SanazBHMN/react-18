ea8046aca0d62faa9030d5f7a354b13b267df3e2
SanazBHMN <sanazbahmani35@gmail.com>
2022-10-29 15:01:15 +0330

    Create an active page link in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 const navigation = [
-  { name: "Employees", href: "/employees", current: true },
-  { name: "Customers", href: "/customers", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Employees", href: "/employees" },
+  { name: "Customers", href: "/customers" },
+  { name: "Projects", href: "/other" },
+  { name: "Calendar", href: "/other2" },
 ];
 
 function classNames(...classes) {
@@ -47,19 +48,26 @@ export default function Example(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "no-underline bg-gray-900 text-white"
-                            : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        to={item.href}
+                        // className={classNames(
+                        //   item.current
+                        //     ? "no-underline bg-gray-900 text-white"
+                        //     : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
+                        //   "px-3 py-2 rounded-md text-sm font-medium"
+                        // )}
+                        className={({ isActive }) => {
+                          return (
+                            "px-3 py-2 rounded-md text-sm font-medium no-underline " +
+                            (isActive
+                              ? "bg-gray-700 text-white"
+                              : "text-gray-300 hover:bg-gray-900 hover:text-white")
+                          );
+                        }}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
